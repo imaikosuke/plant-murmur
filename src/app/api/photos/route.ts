f034c303db7b9077944f26ed00a500ae21bd41e3
imaikosuke <imaikosuke2@gmail.com>
2024-07-26 13:52:17 +0900

    写真をAzureから取得するように修正

--- src/app/api/photos/route.ts
+++ src/app/api/photos/route.ts
@@ -1,6 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
-import { mockPhotos, Photo } from "@/lib/mockPhotos";
+import pool from "@/lib/db";
 
 export async function GET(req: NextRequest) {
-  return NextResponse.json(mockPhotos, { status: 200 });
+  try {
+    const client = await pool.connect();
+    const result = await client.query("SELECT * FROM photos ORDER BY timestamp ASC");
+    client.release();
+    return NextResponse.json(result.rows, { status: 200 });
+  } catch (error) {
+    console.error("Error fetching data from PostgreSQL:", error);
+    return NextResponse.json({ error: "Failed to fetch data" }, { status: 500 });
+  }
 }
